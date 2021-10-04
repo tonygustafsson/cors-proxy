5a088e88a55986f22b5bc4ecd975d9d055dcaa0e
Tony Gustafsson <tony.gustafsson@starrepublic.com>
2021-10-04 15:53:54 +0200

    All origins now works

--- corsProxy.js
+++ corsProxy.js
@@ -4,19 +4,17 @@ const request = require("request");
 const app = express();
 
 /* ------ SETTINGS ------ */
-const siteOrigin = "http://localhost:3000"; // The site URL that is using the API
 const apiOrigin = "https://api.myserver.com"; // The actual API Protocol + domain
-
 const proxyListeningPath = "/api/*"; // "*" will listen for everything and send it to apiOrigin
 const proxyDefaultPort = 3001; // This proxys listening port
-
+const apiTimeout = 60000; // Number of milliseconds to wait for API responses
 const allowCredentials = true; // Needed if credentials are used on site
 const allowHeaders = ["x-xsrf-token", "x-requested-with"]; // The headers that is allowed through
 /* ----------------------- */
 
 app.use((req, res, next) => {
   // Set necessary headers for all requests
-  res.header("Access-Control-Allow-Origin", siteOrigin);
+  res.header("Access-Control-Allow-Origin", req.get("origin"));
   res.header("Access-Control-Allow-Credentials", allowCredentials);
   res.header("Access-Control-Allow-Headers", allowHeaders.join(","));
 
@@ -26,16 +24,19 @@ app.use((req, res, next) => {
 app.get(proxyListeningPath, (req, res) => {
   // Listen for requests for relevant paths
 
-  request({ url: `${apiOrigin}/${req.path}` }, (error, response, body) => {
-    if (error || response.statusCode !== 200) {
-      console.error(`Could not handle ${req.path}. Error: ${error.message}`);
-      return res.status(500).json({ type: "error", message: error.message });
+  request(
+    { url: `${apiOrigin}/${req.path}`, timeout: apiTimeout },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        console.error(`Could not handle ${req.path}. Error: ${error}`);
+        return res.status(500).json({ type: "error", message: error });
+      }
+
+      console.log(`Handling ${req.path}`);
+
+      res.json(JSON.parse(body));
     }
-
-    console.log(`Handling ${req.path}`);
-
-    res.json(JSON.parse(body));
-  });
+  );
 });
 
 const PORT = process.env.PORT || proxyDefaultPort;
